3c86b9694b2ac0afc210873c07d2f97da0218849
Chimgee100 <erdenechimegmergenbaatar@edu.bme.hu>
2025-03-21 15:39:11 +0100

    Add files via upload

--- sketch_mar19c.ino
+++ sketch_mar19c.ino
@@ -1,5 +1,8 @@
 #include <Wire.h>
 #include "SparkFun_SCD30_Arduino_Library.h"
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include "Arduino_SensorKit.h"
 
 SCD30 airSensor;
 
@@ -7,16 +10,39 @@ void setup() {
     Serial.begin(115200);
     Wire.begin();
     Wire.setClock(100000);
+    Oled.begin();
+    Oled.setFlipMode(true);
 
+    
     if (!airSensor.begin(0x61)) {
         Serial.println("SCD30 not detected!");
         while (1);
-    } else {
+    }
+    else {
         Serial.println("SCD30 detected!");
     }
+    delay(2000);
 }
 
 void loop() {
+
+    Oled.setFont(u8x8_font_chroma48medium8_r); 
+    Oled.setCursor(0, 3);
+    Oled.print("Temp: ");
+    Oled.print(airSensor.getTemperature());
+    Oled.println(" C");
+
+    Oled.print("Humi: ");
+    Oled.print(airSensor.getHumidity());
+    Oled.println(" %");
+    
+    Oled.print("CO2: ");
+    Oled.print(airSensor.getCO2());
+    Oled.println(" ppm");
+
+    delay(2000);
+
+
     if (airSensor.dataAvailable()) {
         Serial.print("CO2 (ppm): ");
         Serial.print(airSensor.getCO2());
@@ -27,6 +53,5 @@ void loop() {
     } else {
         Serial.println("Waiting for data...");
     }
-
     delay(2000);
 }
